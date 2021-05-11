d3276f445038e51e31d735c6631767ffe5f2f851
light2077 <435786117@qq.com>
2021-05-11 22:43:35 +0800

    update spider

--- 爬虫/路飞学城 爬虫/demo.py
+++ 爬虫/路飞学城 爬虫/demo.py
@@ -1,53 +1,33 @@
 import requests
 import time
 from lxml import etree
+from chaojiying import ocr
+
+import os
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/90.0.4430.93 Safari/537.36"
 }
+# url = "http://www.renren.com/SysHome.do"
 
+# text = requests.get(url, headers=headers)
+# tree = etree.HTML(text)
 
-def get_html():
-    url = "http://bj.58.com/ershoufang/"
-    text = requests.get(url, headers=headers).text
-    return text
+# code_img_src = tree.xpath('//*[@id="verifyPic_login"]/@src')[0]
+code_path = "code.png"
+session = requests.session()
 
+data = {
+    "email": "15261813289",
+    "password": "renren1996social",
+    "icode": "",
+    "origURL": "http://www.renren.com/home",
+    "domain": "renren.com",
+    "key_id": "1",
+    "captcha_type": "web_login"
+}
 
-# 解析一页的房屋数据
-def parse(html):
-    tree = etree.HTML(html)
-    houses = tree.xpath("//div[@class='property']")
-    for house in houses:
-        data = dict()
-        # 标题
-        data['title'] = house.xpath(".//h3[@class='property-content-title-name']/text()")[0]
-        # 户型
-        unit_type = house.xpath(".//p[@class='property-content-info-text property-content-info-attribute']//text()")
-        data['unit_type'] = "".join(unit_type)
-        # 面积
-        data['aera'] = house.xpath(".//p[@class='property-content-info-text'][1]//text()")[0].strip()
-        # 朝向
-        data['ori'] = house.xpath(".//p[@class='property-content-info-text'][2]//text()")[0].strip()
+resp = session.post("http://www.renren.com/PLogin.do", headers=headers, data=data)
 
-        # 小区名
-        data['community_name'] = house.xpath(".//p[@class='property-content-info-comm-name']//text()")[0].strip()
-        # 小区地址
-        community_addr = house.xpath(".//p[@class='property-content-info-comm-address']//text()")
-        data['community_addr'] = " ".join(community_addr)
-
-        # 总价
-        price = house.xpath(".//p[@class='property-price-total']//text()")
-        data["price"] = "".join(price)
-
-        # 每平米均价
-        data["avg_price"] = house.xpath(".//p[@class='property-price-average']//text()")[0]
-
-        # 链接
-        data["url"] = house.xpath(".//a[@class='property-ex']/@href")[0]
-        print(data['title'])
-        # 把这条数据存入数据库内
-        # save_data()
-
-        time.sleep(1)
