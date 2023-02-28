01b719fbf9a856761c589aba75a420dfcda398f0
light2077 <435786117@qq.com>
2023-03-01 00:00:52 +0800

    update

--- python/pythonGUI/PyQt5/demo.py
+++ python/pythonGUI/PyQt5/demo.py
@@ -1,36 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QWidget, QApplication, QDateTimeEdit, QVBoxLayout
+from PyQt5.QtCore import QDateTime
 
-class Example(QMainWindow):
+class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
+        self.datetime_edit = QDateTimeEdit(QDateTime.currentDateTimeUtc())
+        self.datetime_edit.setCalendarPopup(True)
+        self.datetime_edit.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.datetime_edit)
+        self.setLayout(vbox)
+        # self.setGeometry(300, 300, 350, 250)
+        # self.setWindowTitle('Main window')
 
-        textEdit = QTextEdit()
-        self.setCentralWidget(textEdit)
-
-        exitAct = QAction(QIcon('exit.png'), 'Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(self.close)
-
-        self.statusBar()
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAct)
-
-        toolbar = self.addToolBar('Exit')
-        toolbar.addAction(exitAct)
-
-        self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('Main window')
+        self.datetime_edit.dateTimeChanged.connect(self.show_dt)
         self.show()
+    def show_dt(self, event):
+        print(type(event))  # PyQt5.QtCore.QDateTime
+        print(event)  # PyQt5.QtCore.QDateTime'
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
+ 
